4dd2cf5931fdfd5e46bdb85c583267af08f74546
Sarthak Saraf <sarthak.saraf1@gmail.com>
2023-08-17 01:27:09 +0530

    Delete frappe-frontend directory

--- frappe-frontend/pages/records.js
+++
@@ -1,7 +0,0 @@
-export default function Records(){
-    return(
-        <div>
-            <div className='text-5xl flex items-center justify-center m-10 font-bold'>Records</div>
-        </div>
-    )
-}
